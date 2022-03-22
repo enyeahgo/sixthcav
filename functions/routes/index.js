115d0b44b9b62217945e3f92637661a006b45e9e
Inigo Orosco <enyeahgo@gmail.com>
2022-03-22 12:20:12 +0800

    New Commit

--- functions/routes/index.js
+++ functions/routes/index.js
@@ -2,7 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('cod');
+  res.render('cod', {
+    isLoggedIn: true,
+    title: [
+      "Call Of Duty Mobile",
+      "Get Ready For Battle",
+      "Prepare Yourself"
+    ],
+    message: [
+      "Welcome to CODM Hub!",
+      "Join with your friends",
+      "Enjoy the exhilarating games"
+    ]
+  });
 });
 
 module.exports = router;
