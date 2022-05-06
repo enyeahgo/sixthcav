d2ec17a251af9fa39f6dc37866f43322e3bf1094
Inigo Orosco <enyeahgo@gmail.com>
2022-05-06 14:27:45 +0800

    New Commit

--- functions/routes/index.js
+++ functions/routes/index.js
@@ -2,18 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('cod', {
-    isLoggedIn: true,
-    title: [
-      "Call Of Duty Mobile",
-      "Get Ready For Battle",
-      "Prepare Yourself"
-    ],
-    message: [
-      "Welcome to CODM Hub!",
-      "Join with your friends",
-      "Enjoy the exhilarating games"
-    ]
+  res.render('index', {
+    title: '6CAVCOY DASHBOARD'
   });
 });
 
--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,14 +1,14 @@
-importScripts('https://www.gstatic.com/firebasejs/8.2.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.2.1/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging.js');
 var firebaseConfig = {
-  apiKey: "AIzaSyCQ4zInvNZYWQsDQAUBttx8hqpW9Z6Fd68",
-  authDomain: "tayaph.firebaseapp.com",
-  databaseURL: "https://tayaph-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "tayaph",
-  storageBucket: "tayaph.appspot.com",
-  messagingSenderId: "1036036432312",
-  appId: "1:1036036432312:web:17776cd4e0c00a1fc3af4d",
-  measurementId: "G-BB64DNC58K"
+  apiKey: "AIzaSyB1CzIfFtTvnVNS2JTJzv4NCGGiQwpAHu8",
+  authDomain: "sixthcav.firebaseapp.com",
+  databaseURL: "https://sixthcav-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "sixthcav",
+  storageBucket: "sixthcav.appspot.com",
+  messagingSenderId: "518529957907",
+  appId: "1:518529957907:web:ed171c2024750395337fe6",
+  measurementId: "G-H641BN2ZFM"
 };
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
@@ -16,8 +16,8 @@ messaging.setBackgroundMessageHandler(function(payload) {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
   const notificationTitle = "TayaPh";
   const notificationOptions = {
-    body: "You have a new important notification in TayaPh",
-    click_action: "https://tayaph.web.app"
+    body: "You have a new important notification in SixthCav",
+    click_action: "https://sixthcav.web.app"
   };
   return self.registration.showNotification(notificationTitle,
     notificationOptions);
